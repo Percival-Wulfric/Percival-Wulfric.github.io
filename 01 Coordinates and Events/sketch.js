d849b9963b6ffabf36aa458905fe2bd7c7da9530
Percival-Wulfric <7011264@spsd.sk.ca>
2026-02-06 12:18:02 -0600

    Completed cordinate practice

--- 01 Coordinates and Events/sketch.js
+++ 01 Coordinates and Events/sketch.js
@@ -9,15 +9,55 @@
 //            targiting 60 times per second
 //           schreen is updated at bottom of draw
 
+//-------------------------Global Varable Section-------------------------
+// we ca DECLARE variables here
+// we can INITAILIZE variables with
+//    simple data types
+//    > don't have accesss to system variables
+let circleX = 0;
+let circley = 200;
+let SPEED = 2;
+let die = 30;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 400);
 }
 
 function draw() {
+  // repeats over and over (automaticaly) 60fps
+  // aim to keep this TIDY
   background(0); // wipes the screen
-
-  //     x    y  die
-  circle(0, 0, 50);
+  //drawTwoCircles();
+  fiveCircles();
+  movingCircle();
 }
 
+function movingCircle(){
+  circle(circleX, circley, die);
+  circleX = circleX + SPEED;
+  if(circleX > width){
+    circleX = 0;
+  }
+}
+
+function fiveCircles(){
+  let die = 25
+  circle(width/2, height/2, die);
+  circle(width, height, die);
+  circle(width, 0, die);
+  circle(0, height, die);
+  circle(0, 0, die);
+}
+
+function drawTwoCircles(){
+    //    R    G    B
+    fill(200,  100, 20)  // Sets fill color untill updated
+    stroke("#FFFFFF")
+    //     x         y   die
+    circle(circleX, 100, 50);
+
+    // SECOND CIRCLE
+    fill("red")
+    noStroke(); // noFill();
+    circle(width/2, height/2, 200)
+}

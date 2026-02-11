63fcf047252d3724853367d52194b348323ba607
Percival-Wulfric <7011264@spsd.sk.ca>
2026-02-11 12:17:55 -0600

    Interactive Scene

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,18 @@
+// Interactive Scene
+// Muhammad Ismail
+// 2/11/2026
+// 
+
+// Globle variable
+let bgc = "";
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  
+  background(255);
+}

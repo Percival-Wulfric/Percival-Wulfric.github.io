d37d457c19bc7c09a59715fd0049fa636981538f
Percival-Wulfric <7011264@spsd.sk.ca>
2026-02-13 12:17:36 -0600

    addied a sun

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -12,7 +12,18 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function drawMountain(){
+  // This function will make a mountain
+  triangle(width/2, 0, width/4, height - 20, width * 3/4, height - 20);
+}
+
+function sun(){
+  // this function makes a sun
+  circle(mouseX, 55, 100);
+}
 function draw() {
-  
+
   background(255);
+  drawMountain();
+  sun();
 }

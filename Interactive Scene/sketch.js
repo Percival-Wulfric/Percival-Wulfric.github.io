cf77ab7bad89aa19819fdedd90781f2891fb0a72
Percival-Wulfric <7011264@spsd.sk.ca>
2026-02-21 15:14:23 -0600

    Updated intractive story
    I added the tree and mountains a cool sun and ground, left couments for things i still whant to add

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -4,26 +4,90 @@
 // 
 
 // Globle variable
-let bgc = "";
-
+let x = 395;
+let y = 700;
+let ySpeed = 0;
+let GRAVITY = 0.7;
+let currentBack = 0;
+let ground = 795;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  createCanvas(ground, 950);
 }
 
-function drawMountain(){
+function drawMountain(x, peak, size){
   // This function will make a mountain
-  triangle(width/2, 0, width/4, height - 20, width * 3/4, height - 20);
+
+  fill('#5c5c5c');
+  triangle(x, peak, x - size/2, ground, x + size/2, ground);
+
 }
 
 function sun(){
   // this function makes a sun
-  circle(mouseX, 55, 100);
+  fill('#ffd16622');
+  circle(mouseX, mouseY, 100);
+
+  fill('#ffd60a');
+  circle(mouseX, mouseY, 50);
 }
+
+function drawTree(x,y){
+  // This function will draw a tree
+
+  fill('#6b3e1d');
+  rect(x - 6, y - 70, 12, 70);
+
+  fill('#2a9d23');
+  triangle(x, y - 140, x - 35, y - 70, x + 35, y - 70);
+
+  fill('#1f7d1f');
+  triangle(x, y - 110, x - 30, y - 50, x + 30, y - 50);
+}
+
+function character(x, y){
+  // draws my charcter
+
+}
+
+function mousePressed() {
+  // This is a existing function in teh p5.js library
+  if (mouseButton == CENTER) {
+    // Update bacground
+
+  }
+
+}
+
 function draw() {
 
+  // change current back state to change color
+
   background(255);
-  drawMountain();
   sun();
+
+  // Mountains 
+  drawMountain(200, 350, 350);
+  drawMountain(580, 280, 380);
+
+  // Ground
+  fill('#3a9d23');
+  rect(0, ground, 795, 200);
+
+  // Trees
+  drawTree(120, ground);
+  drawTree(690, ground);
+  
+  // Name
+  fill(0);
+  text("Muhammad Ismail", 15, 915);
+
+  // Update x and y to move the charcter
+
+  // Charcter
+  character(x, y);
+  
+  
 }

16f6ae4c02c94e880c6c4352ac19b14c6eb7b309
Percival-Wulfric <7011264@spsd.sk.ca>
2026-03-04 12:17:13 -0600

    Challenge Feature - Mountain Top
    I finished the challenge feature to find the peak and make a flag, will try and do the panning later today

--- Perlin Noise Project (Terrain Generation)/sketch.js
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -7,6 +7,7 @@
 // Globle Variable's
 let rectangle_width = 2;
 let ogStartTime, startTime;
+let peak_x, peak_y;
 
 
 function setup() {
@@ -29,9 +30,17 @@ function keyPressed() {
   }
 }
 
+function drawFlag(x, y){
+  // This function makes a flag at cordinates x,y
+  rect(x,y, 3, 30);
+  triangle(x + 3, y, x+3, y+12, x+3+11, y +5 );
+}
+
 function generateTerrain(){
   // w → is the width of the rectangles
   // This function will genrate terain
+  peak_x = 0;
+  peak_y = 0;
 
   startTime = ogStartTime;
   let rectHeight = 0;
@@ -39,7 +48,11 @@ function generateTerrain(){
 
   for (let x = 0; x + rectangle_width < width; x += rectangle_width){
     // This loop makes each rectangle
-    rectHeight = map(noise(startTime), 0, 1, 0, height*0.75);
+    rectHeight = map(noise(startTime), 0, 1, 0, height*0.95);
+    if (peak_y < rectHeight){
+      peak_y = rectHeight;
+      peak_x = x;
+    }
     rect(x, height, rectangle_width, rectHeight*-1);
     startTime += timeChange;
   }
@@ -50,5 +63,7 @@ function generateTerrain(){
 function draw() {
   background(220);
   generateTerrain();
-  startTime = ogStartTime; 
+  fill(255,0,0);
+  drawFlag(peak_x, height - peak_y - 30);
+  fill(0);
 }

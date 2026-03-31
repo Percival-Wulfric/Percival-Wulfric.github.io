83a9953d2ffd460ad5099fcc66abd24dfeec0bad
Percival-Wulfric <7011264@spsd.sk.ca>
2026-03-30 20:35:11 -0600

    Probobly finished cars assignment
    I think I did evreything, the cars, the adding of cars and trafic light

--- Projects/Cars Cars Cars/sketch.js
+++ Projects/Cars Cars Cars/sketch.js
@@ -6,14 +6,53 @@
 // Globles
 const LINEDASHVAL = 30;
 const MAXSPEED = 15;
-let x;
-let r;
+let eastbound = []; let westbound = [];
+let r; let lightColor = 1; // 1 = green light, 0 = red light
+let c = [0, 255, 0]; let d;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = new Vehicle(width/2, height/2);
-  
+  for(let i = 0; i < 20; i++){
+    eastbound.push(new Vehicle(250, random(height/4 *2 + 20,  height/4 *3 - 25), 1)); 
+  }
+  for(let n = 0; n < 20; n++){
+    westbound.push(new Vehicle(250, random(height/2 - 25, height/4 + 5), 0)); 
+  }
+
 }
+
+function mouseClicked(){
+  // This is a built in function which i am utalising make the traficl light turn 
+  // red and back to green when i click the circle
+  
+  if(d < 30) {
+    // 30 = diameter of circle
+    if(!(lightColor)) lightColor = 1;
+    else lightColor = 0;
+  }
+  
+  // For more cars
+  
+  if(keyIsDown(SHIFT)) westbound.push(new Vehicle(250, random(height/2 - 25, height/4 + 5), 0));
+  else eastbound.push(new Vehicle(250, random(height/4 *2 + 20,  height/4 *3 - 25), 1));
+
+  //console.log(westbound.length, eastbound.length);
+}
+
+function traficLight(x, y){
+  // This function will make a working trafic light that if you clic on 
+  // will turn the lights red and then back to green
+  fill(c[0],c[1], c[2]);
+  circle(x, y, 60);
+
+  d = dist(mouseX, mouseY, x, y); // this finds the distance betwen the first 2 and last 2 numbers
+  //console.log(d);
+  
+
+  if(lightColor) c = [0, 255, 0];
+  else c = [255, 0, 0];
+}
+
 
 function drawRoad(){
 
@@ -35,11 +74,11 @@ function setLineDash(list) {
 class Vehicle{
   // Consttuctor
 
-  constructor(x, y){
+  constructor(x, y, d){
     this.type = Math.floor(random(2));
-    this.color = [random(255),random(255),random(255)];
+    this.color = [random(254),random(254),random(254)];
     this.x = x; this.y = y;
-    this.direction = Math.floor(random(2));
+    this.direction = d // - 0 is right to left ←,  1 is left to right →
     this.xSpeed = Math.floor(random(1,MAXSPEED));
 
   }
@@ -108,16 +147,19 @@ class Vehicle{
 
   action(){
     this.display();
-    this.move();
 
-    // Random Number
-    r = int(random(101));
-    if(r === 1 || r === 2 || r === 3) console.log(r);
-    
-    // Speed up or Speed down
-    if(r === 1) this.speedUp();
-    else if (r === 2) this.speedDown();
-    else if (r === 3) this.changeColor();
+    if(lightColor){ // Bulian value of 0 is false and 1 is true
+          this.move();
+
+      // Random Number
+      r = int(random(101));
+      
+      // Speed up or Speed down
+      if(r === 1) this.speedUp();
+      else if (r === 2) this.speedDown();
+      else if (r === 3) this.changeColor();
+    }
+
   }
 
 
@@ -126,6 +168,11 @@ class Vehicle{
 function draw() {
   background(255);
   drawRoad();
-  x.action();
-  
+  for(let i in eastbound){
+    eastbound[i].action();
+  }
+  for(let n in westbound){
+    westbound[n].action();
+  }
+  traficLight(100, 100);
 }

4b5d9f582f4f5e44da6f1b53fd92bf9de855ab37
Percival-Wulfric <7011264@spsd.sk.ca>
2026-04-16 12:17:17 -0600

    finishing demo and starting the project
    the project is an addon to the demo so i did it in the same file i will move it to the projects file or have duplicate. I will diside lator

--- Demos/19 Two D aray basics/sketch.js
+++ Demos/19 Two D aray basics/sketch.js
@@ -16,19 +16,30 @@ let grid = [
 let rows = grid.length;
 let cols = grid[0].length;
 let tileSize = 150;
+let winStatus;
+let slectType = 0; // 0 → crows, 1 → box
+
+function makeGrid(){
+  // this function will make a random custom grid
+}
 
 function setup() {
   createCanvas(cols*tileSize, rows*tileSize);
   //noStroke();
   textSize(30);
+  noStroke();
 }
 
 function draw() {
   background(200);
+  winStatus = winCondition()
   renderGrid(grid, rows, cols, tileSize);
   textSize(30);
   fill(255, 0, 0);
   text(getCurrentX() + ", " +  getCurrentY(), mouseX, mouseY);
+  noStroke();
+
+  if(winStatus) win();
   
 }
 
@@ -39,7 +50,43 @@ function flip(x, y){
 }
 
 function mousePressed(){
-  flip(getCurrentX(), getCurrentY())
+  if(!winStatus){
+      // only do a flip if mouse is on the canves
+    if(mouseX < width && mouseY < height){
+      let x = getCurrentX();
+      let y = getCurrentY();
+
+      if(keyIsDown(SHIFT)){
+        // ALWAYS FLIP THE CURENT TILE
+        flip(x, y);
+      }
+      
+      else{
+        flip(x, y);
+
+        // If they exist:
+        // flip the cordinal (NSEW) naigbours
+        if(x-1 >= 0) flip(x-1, y);
+        if(x+1 <= cols-1) flip(x+1, y);
+        if(y-1 >= 0) flip(x, y-1);
+        if(y+1 <= rows-1) flip(x, y+1);
+        //if(y >= 0 && y)
+      }
+      
+    }
+  }
+}
+
+function slection(){
+  // this function will tell me all boxes effected
+  if(slectType){
+    // Box slection mood
+    
+  }
+  else{
+    // Crows slection mood
+
+  }
 }
 
 function renderGrid(grid, rows, cols, tileSize){
@@ -69,4 +116,32 @@ function getCurrentY(){
   // Determent the current culome position of mouse
   let constranedY = constrain(mouseY, 0, height-1);
   return floor(constranedY / tileSize);
+}
+
+function winCondition(){
+  let previous;
+  let current;
+  let count = 0;
+  for(let y = 0; y < rows; y++){ // y: 0, 1, 2, 3, 4
+    for(let x = 0; x < cols; x++){// x: 0, 1, 2, 3, 4, 5
+      if(x > 0){
+        previous = current;
+        current = grid[y][x];
+
+        if(current === previous) count ++;
+      }
+      else current = grid[y][x];
+
+    }
+  }
+
+  if(count < rows * (cols-1)) return false;
+  return true;
+}
+
+function win(){
+  strokeWeight(3);
+  fill(255,0,0);
+  text("You have won", width/2, height/2);
+  noStroke();
 }

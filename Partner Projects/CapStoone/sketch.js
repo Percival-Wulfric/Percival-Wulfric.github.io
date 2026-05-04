08c1d9a0660bb02e6dad0970467e760584a27077
Percival-Wulfric <7011264@spsd.sk.ca>
2026-05-04 12:17:39 -0600

    cAPSTOON
    Start

---
+++ Partner Projects/CapStoone/sketch.js
@@ -0,0 +1,24 @@
+// Tag Game
+// Ayeman & Muhammad
+// May 4/26
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+class player{
+  constructor(){
+
+  }
+
+  movement(){
+
+  }
+
+  
+} 
